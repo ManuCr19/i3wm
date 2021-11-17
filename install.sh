7b6ded0e03e9f456531fbad7e8031a996c920180
ManuCr19 <82413811+ManuCr19@users.noreply.github.com>
2021-11-17 09:31:53 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@
 sudo pacman -S i3-gaps i3blocks rofi feh numlockx pavucontrol blueberry
 
 # Comprobar que existe la carpeta .config, si no existe la crea
-ls $HOME/.config >/deb/null 2>/dev/null || mkdir $HOME/.config
+ls $HOME/.config >/dev/null 2>/dev/null || mkdir $HOME/.config
 
 # Copiar la configuracion a la carpeta home del usuario
 cp -r i3 $HOME/.config
