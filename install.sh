0231401a5ee63bdc23025898b741e5af5b657b94
ManuCr19 <82413811+ManuCr19@users.noreply.github.com>
2021-11-17 19:03:04 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,6 +5,11 @@ sudo pacman -S i3-gaps i3blocks rofi feh numlockx pavucontrol blueberry
 
 # Comprobar que existe la carpeta .config, si no existe la crea
 ls $HOME/.config >/dev/null 2>/dev/null || mkdir $HOME/.config
+ls /etc/skel/.config >/dev/null 2>/dev/null || mkdir /etc/skel/.config
+
 
 # Copiar la configuracion a la carpeta home del usuario
 cp -r i3 $HOME/.config
+
+# Copiar la configuracion al skel
+sudo cp -r i3 /etc/skel/.config
