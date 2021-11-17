a303b28df8d43b0716d4d3982910f388471afeaa
ManuCr19 <82413811+ManuCr19@users.noreply.github.com>
2021-11-17 09:17:47 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
+# Instalar paquetes necesarios en Arch Linux
 sudo pacman -S i3-gaps i3blocks rofi feh numlockx pavucontrol blueberry
+
+# Copiar la configuracion a la carpeta home del usuario
+cp -r i3 $HOME/.config/
