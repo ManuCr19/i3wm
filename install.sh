5d14bbe852f4c1ea30fac4ff5dc5bc155a3e6ff4
ManuCr19 <82413811+ManuCr19@users.noreply.github.com>
2021-11-19 10:16:31 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,15 +6,9 @@ sudo pacman --noconfirm -Sy i3-gaps i3blocks rofi feh numlockx pavucontrol blueb
 # Instalar programas
 trizen --noconfirm -Sy brave-bin nautilus libreoffice-fresh-es hunspell-es_es steam alacritty gedit
 
-# Comprobar que existe la carpeta .config, si no existe la crea
-ls $HOME/.config >/dev/null 2>/dev/null || mkdir $HOME/.config
+# Comprobar que existe la carpeta /etc/skel/.config/i3, si no existe la crea
 ls /etc/skel/.config >/dev/null 2>/dev/null || mkdir /etc/skel/.config
-
-# Añadimos permisos de ejecucion a los bloques
-chmod +x i3/blocks/*
-
-# Copiar la configuracion a la carpeta home del usuario
-cp -r i3 $HOME/.config
+ls /etc/skel/.config/i3 >/dev/null 2>/dev/null || mkdir /etc/skel/.config/i3
 
 # Copiar la configuracion al skel
-sudo cp -r i3 /etc/skel/.config
+sudo cp -r i3/* /etc/skel/.config/i3
