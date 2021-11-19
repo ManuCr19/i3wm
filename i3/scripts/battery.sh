3144d5ec39743d28575aa09d94749a4dae62de46
ManuCr19 <82413811+ManuCr19@users.noreply.github.com>
2021-11-19 16:51:42 +0100

    Update battery.sh

--- i3/scripts/battery.sh
+++ i3/scripts/battery.sh
@@ -8,6 +8,6 @@ acpi | grep Charging >/dev/null 2>&1 && STATE=connect
 BAT=$(acpi | awk -F, '{ print $2 }')
 
 case $STATE in
-  disconnect) echo "$BAT" ;;
-  connect) echo "$BAT" ;;
+  disconnect) echo " $BAT" ;;
+  connect) echo " $BAT" ;;
 esac
