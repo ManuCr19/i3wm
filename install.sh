99797ab8addb7d69118401296616ba5af41428f2
ManuCr19 <82413811+ManuCr19@users.noreply.github.com>
2021-11-19 10:27:49 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,9 +6,7 @@ sudo pacman --noconfirm -Sy i3-gaps i3blocks rofi feh numlockx pavucontrol blueb
 # Instalar programas
 trizen --noconfirm -Sy brave-bin nautilus libreoffice-fresh-es hunspell-es_es steam alacritty gedit
 
-# Comprobar que existe la carpeta /etc/skel/.config/i3, si no existe la crea
-ls /etc/skel/.config >/dev/null 2>/dev/null || sudo mkdir /etc/skel/.config
-ls /etc/skel/.config/i3 >/dev/null 2>/dev/null || sudo mkdir /etc/skel/.config/i3
-
 # Copiar la configuracion al skel
+sudo mkdir /etc/skel/.config
+sudo mkdir /etc/skel/.config/i3
 sudo cp -r i3/* /etc/skel/.config/i3
