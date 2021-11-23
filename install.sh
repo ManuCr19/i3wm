1323183cb8082f94910643df3fa1ff5db4a60199
ManuCr19 <82413811+ManuCr19@users.noreply.github.com>
2021-11-23 19:58:00 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Instalar paquetes necesarios en Arch Linux
-sudo pacman --noconfirm -Sy i3-gaps i3blocks rofi feh numlockx ttf-font-awesome acpi playerctl pacman-contrib
+sudo pacman --noconfirm -Sy i3-gaps i3blocks rofi feh numlockx ttf-font-awesome acpi playerctl pacman-contrib brightnessctl
 
 # Instalar programas
 trizen --noconfirm -Sy brave-bin nautilus libreoffice-fresh-es hunspell-es_es steam alacritty gedit
