7dcc54365e09d62e6a47020b7ac449293c61c083
ManuCr19 <82413811+ManuCr19@users.noreply.github.com>
2021-11-25 09:38:12 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,7 +6,7 @@ sudo systemctl enable lightdm.service
 sudo cp lightdm/19-i3wm-ManuCr19.conf /usr/share/lightdm/lightdm.conf.d
 
 # Instalar paquetes necesarios en Arch Linux
-sudo pacman --noconfirm -Sy i3-gaps i3blocks rofi feh numlockx ttf-font-awesome acpi playerctl pacman-contrib brightnessctl pulseaudio pavucontrol
+sudo pacman --noconfirm -Sy i3-gaps i3blocks rofi feh numlockx ttf-font-awesome acpi playerctl pacman-contrib brightnessctl pulseaudio pavucontrol dunst
 
 # Instalar programas
 trizen --noconfirm -Sy brave-bin nautilus libreoffice-fresh-es hunspell-es_es steam alacritty gedit
