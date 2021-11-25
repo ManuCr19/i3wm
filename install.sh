7573cbbd38e8f60ef7ed8502bd7306901d727fb5
ManuCr19 <82413811+ManuCr19@users.noreply.github.com>
2021-11-25 09:20:53 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,6 +3,7 @@
 # Instalar gestor de inicio de sesion
 sudo pacman --noconfirm -Sy lightdm slick-greeter materia-gtk-theme
 sudo systemctl enable lightdm.service
+sudo cp lightdm/19-i3wm-ManuCr19.conf /usr/share/lightdm/lightdm.conf.d
 
 # Instalar paquetes necesarios en Arch Linux
 sudo pacman --noconfirm -Sy i3-gaps i3blocks rofi feh numlockx ttf-font-awesome acpi playerctl pacman-contrib brightnessctl pulseaudio pavucontrol
