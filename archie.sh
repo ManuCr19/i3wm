1edcb5d4812d7ddf87a947430085f5a8ebf816af
ManuCr19 <82413811+ManuCr19@users.noreply.github.com>
2021-12-07 17:11:07 +0100

    Update archie.sh

--- archie.sh
+++ archie.sh
@@ -18,7 +18,7 @@ sudo rm -r rofi-themes
 sudo pacman --noconfirm -Sy gnome-themes-extra
 
 # Instalar paquetes necesarios
-sudo pacman --noconfirm -Sy i3-gaps i3blocks rofi feh numlockx ttf-font-awesome acpi playerctl pacman-contrib brightnessctl pulseaudio pavucontrol dunst network-manager-applet
+sudo pacman --noconfirm -Sy i3-gaps i3blocks rofi feh numlockx ttf-font-awesome acpi playerctl pacman-contrib brightnessctl pulseaudio pavucontrol dunst network-manager-applet blueman
 
 # Instalar programas
 trizen --noconfirm -Sy brave-bin nautilus libreoffice-fresh-es hunspell-es_es alacritty lutris
