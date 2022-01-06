4ba098bde9b1fee88998e65852c9613a4f9f9fe2
ManuCr19 <82413811+ManuCr19@users.noreply.github.com>
2022-01-06 20:59:02 +0100

    Update archie.sh

--- archie.sh
+++ archie.sh
@@ -6,6 +6,8 @@ sudo systemctl enable lightdm.service
 sudo cp lightdm/slick-greeter.conf /etc/lightdm
 sudo sed -i '/#greeter-session/c greeter-session=lightdm-slick-greeter' /etc/lightdm/lightdm.conf
 sudo sed -i '/^greeter-session/c greeter-session=lightdm-slick-greeter' /etc/lightdm/lightdm.conf
+sudo sed -i '/#greeter-show-manual-login/c greeter-show-manual-login=true' /etc/lightdm/lightdm.conf
+sudo sed -i '/^greeter-show-manual-login/c greeter-show-manual-login=true' /etc/lightdm/lightdm.conf
 sudo mkdir /usr/share/backgrounds
 sudo cp lightdm/playa-de-las-catedrales.jpg /usr/share/backgrounds
 
